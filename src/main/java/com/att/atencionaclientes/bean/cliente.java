5a2aba92b39c3bbffb9b8b337e0c5288bcfaea69
msiete <mfsiete@gmail.com>
2017-08-07 11:29:11 -0500

    Mi primer commit

---
+++ src/main/java/com/att/atencionaclientes/bean/cliente.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.att.atencionaclientes.bean;
+
+/**
+ *
+ * @author Java
+ */
+public class cliente {
+    public String nombre;
+    public String apellidos;
+    public int edad;
+    public float saldo;
+    
+    public cliente(){
+    
+    }
+    
+}
